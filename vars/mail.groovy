d001c22b196aecf36db96c2fa7b3a985c0b7e6aa
Somya Gaur <39803705+Somyagaur06@users.noreply.github.com>
2020-09-24 01:20:42 +0530

    Update mail.groovy

--- vars/mail.groovy
+++ vars/mail.groovy
@@ -1,11 +1,14 @@
 
-def call(String name='demo'){
+def call(Map parameter){
   
   
   pipeline{
     
     agent any
-   
+    environment{
+    
+    PATH="/usr/share/maven/bin/:$PATH"
+ }
  
 
     
@@ -16,16 +19,69 @@ def call(String name='demo'){
            
            steps{
                
-               mail bcc: '', body: '''Hi,
-
-This is to inform you that your recent build is success.''', cc: '', from: '', replyTo: '', subject: 'Jenkins Build Success ', to: 'gaursomya@yahoo.com'
-          
+               git 'https://github.com/Somyagaur06/jenkinsDemo.git'
                 }
           }
           
           
-    }}
+          stage("Maven Build")
+          {
+          
+          steps{
+   
+     dir('/var/lib/jenkins/workspace/sharedLibraryDemo/jenkinsDemo'){
   
+         sh "mvn clean package"
+         
+          }
+          
+          }
+          }
+          
+           stage("Tomcat-Deployment over SSH"){
+          
+          steps{
+          
+          sshagent(['deploy_user']){
+          
+         sh " scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sharedLibraryDemo/jenkinsDemo/target/*.war ubuntu@172.31.1.78:/opt/apache-tomcat/webapps "
+
+          
+          }
+           }
+          }
+          
+          
+          
+          }
+          
+          post{
+           failure{
+          email 'fail'
+          }
+          
+          
+          success{
+          
+        email 'success'
+          
+          
+          }
+          }
+          
+ 
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   
   
   
