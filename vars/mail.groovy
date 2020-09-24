50234d4a2afdaedff151275c46aa15cd9d33f0e7
Somya Gaur <39803705+Somyagaur06@users.noreply.github.com>
2020-09-24 09:41:10 +0530

    Update mail.groovy

--- vars/mail.groovy
+++ vars/mail.groovy
@@ -17,37 +17,37 @@ def call(Map parameter){
        stage("Git checkout")
        {
            
-           steps{
+          
                
                git 'https://github.com/Somyagaur06/jenkinsDemo.git'
                 }
-          }
+          
           
           
           stage("Maven Build")
           {
           
-          steps{
+          
    
      dir('/var/lib/jenkins/workspace/sharedLibraryDemo/jenkinsDemo'){
   
          sh "mvn clean package"
          
-          }
           
-          }
+          
+     }
           }
           
            stage("Tomcat-Deployment over SSH"){
           
-          steps{
+          
           
           sshagent(['deploy_user']){
           
          sh " scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sharedLibraryDemo/jenkinsDemo/target/*.war ubuntu@172.31.1.78:/opt/apache-tomcat/webapps "
 
           
-          }
+          
            }
           }
           
