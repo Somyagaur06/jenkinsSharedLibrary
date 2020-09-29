40d63d676a39e4b5c44e32b7194ff34da40c7a7d
Somya Gaur <39803705+Somyagaur06@users.noreply.github.com>
2020-09-30 00:36:44 +0530

    Create ansibleDemo.groovy

---
+++ vars/ansibleDemo.groovy
@@ -0,0 +1,99 @@
+def call(){
+
+
+node{
+ 
+  
+
+
+ stage("Git Checkout")
+ {
+ 
+   git 'https://github.com/Somyagaur06/jenkinsDemo.git'
+ 
+ 
+ }
+
+
+
+stage("Maven Build"){
+
+ dir("/var/lib/jenkins/workspace/sharedLibrary2/jenkinsDemo"){
+
+docker.image('maven:3.3.3-jdk-8').inside{
+ 
+ sh "mvn clean package"
+
+}}}
+
+ 
+ stage("Tomcat Deployment by Ansible"){
+  
+  ansiblePlaybook credentialsId: 'ubuntu', disableHostKeyChecking: true, installation: 'ansible', inventory: 'inventory.inv', playbook: 'tomcatDeploy.yaml'
+ }
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
